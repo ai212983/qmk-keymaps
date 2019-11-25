55b3abc7ea9f3d21b2e4205fd8986c4a84b35d74
Dimitri Fedorov <df.41356@gmail.com>
2019-11-25 14:45:17 +0100

    adding right arrow to hyper for autocompletion

--- planck_ez/keymap.c
+++ planck_ez/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,------------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  |   \   |
  * |------+------+------+------+------+------+------+------+------+------+------+-------|
- * |HprEsc|   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |   "   |
+ * |HprRgt|   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |   "   |
  * |------+------+------+------+------+------+------+------+------+------+------+-------|
  * |Shift |   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |       |
  * |------+------+------+------+------+------+------+------+------+------+------+-------|
@@ -54,10 +54,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_COLEMAK] = LAYOUT_planck_grid(
-    KC_TAB,         KC_Q,    KC_W,           KC_F,            KC_P,  KC_G,   KC_J,           KC_L,           KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
-    HYPR_T(KC_ESC), KC_A,    KC_R,           KC_S,            KC_T,  KC_D,   KC_H,           KC_N,           KC_E,    KC_I,    KC_O,    KC_QUOT,
-    KC_LSFT,        KC_Z,    KC_X,           KC_C,            KC_V,  KC_B,   KC_K,           KC_M,           KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
-    XXXXXXX,        XXXXXXX, LGUI_T(KC_DEL), LALT_T(KC_BSPC), LOWER, KC_SPC, KC_SPC, RAISE,  RCTL_T(KC_ENT), XXXXXXX, XXXXXXX, XXXXXXX
+    KC_TAB,          KC_Q,    KC_W,           KC_F,            KC_P,  KC_G,   KC_J,           KC_L,           KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+    HYPR_T(KC_RGHT), KC_A,    KC_R,           KC_S,            KC_T,  KC_D,   KC_H,           KC_N,           KC_E,    KC_I,    KC_O,    KC_QUOT,
+    KC_LSFT,         KC_Z,    KC_X,           KC_C,            KC_V,  KC_B,   KC_K,           KC_M,           KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+    XXXXXXX,         XXXXXXX, LGUI_T(KC_DEL), LALT_T(KC_BSPC), LOWER, KC_SPC, KC_SPC, RAISE,  RCTL_T(KC_ENT), XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 
