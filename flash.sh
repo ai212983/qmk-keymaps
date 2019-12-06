f19dc93b941257efe5fbab08926d8a4afb468380
Dimitri Fedorov <df.41356@gmail.com>
2019-12-06 12:17:59 +0100

    new repo structure

---
+++ flash.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+source properties.sh
+
+echo "\n = Building and flashing layout '$TARGET_LAYOUT', firmware folder '$QMK_FIRMWARE'\n"
+./build.sh $TARGET_LAYOUT
+./bin/wally-cli $QMK_FIRMWARE/${TARGET_LAYOUT}_ai212983.bin
+
---
+++ properties.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+QMK_FIRMWARE=qmk_firmware
+TARGET_LAYOUT=${1%/} # first argument without last slash
