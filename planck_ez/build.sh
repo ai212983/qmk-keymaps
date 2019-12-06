c4ec83424d0f4a68b5b9094875f5c2257ccf0df0
Dimitri Fedorov <df.41356@gmail.com>
2019-12-06 12:22:19 +0100

    remove old build shell script

--- planck_ez/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rsync -avh ./{config.h,keymap.c,rules.mk} $QMK_FIRMWARE/keyboards/planck/keymaps/ai212983/ 
-rsync -avh --delete ../common/ $QMK_FIRMWARE/users/ai212983/
-cd $QMK_FIRMWARE 
-make planck/ez:ai212983
-wally-cli $QMK_FIRMWARE/planck_ez_ai212093.bin
