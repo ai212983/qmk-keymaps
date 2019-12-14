709d716f90a0a426044f17148af6aa550ff8d6f5
Dimitri Fedorov <df.41356@gmail.com>
2019-12-14 15:58:17 +0100

    fix missed comma typo

--- planck_ez/keymap.c
+++ planck_ez/keymap.c
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, PRV_SPC, NXT_SPC, MSN_CTL
+    XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, PRV_SPC, NXT_SPC, MSN_CTL,
     KC_HYPR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_AMPR, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX,
     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_ASTR, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX,
     _______, XXXXXXX, KC_LCTL, KC_LGUI, _______, XXXXXXX, XXXXXXX, _______, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX
