77b1441ad870fd966e0e43d6e8d88998661715e5
Dimitri Fedorov <df.41356@gmail.com>
2020-01-24 16:58:38 +0100

    add Alt+Enter for planck, update schemas

--- planck_ez/keymap.c
+++ planck_ez/keymap.c
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 
-/* Lower, lockable - navigation and numpad
+/* Lower - navigation and numpad
  * ,-----------------------------------------------------------------------------------.
  * |      |      | Home |  Up  | End  | PgUp |   %  |   7  |   8  |   9  |   *  |  /   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -106,15 +106,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |LShift|      |      |      |      |      |   0  |   1  |   2  |   3  |   +  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      | ^/Del| ⌘/Esc|██████|   Meh/Tab   |RAISpc|⌥/Ent |   .  |  Bsp |  =   |
+ * |      |      | ^/Del| ⌘/Esc|██████|   Meh/Tab   |RAISpc|⌥+Ent |   .  |  Bsp |  =   |
  * `-----------------------------------------------------------------------------------'
  * Ctrl is shifted for better reachability by pinky
  */
 [_LOWER] = LAYOUT_planck_grid(
-    _______, XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_PERC, KC_7,    KC_8,    KC_9,   KC_ASTR, KC_SLSH,
-    _______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, KC_4,    KC_5,    KC_6,   KC_MINS, XXXXXXX,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_0,    KC_1,    KC_2,    KC_3,   KC_PLUS, XXXXXXX,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DOT, KC_BSPC, KC_EQL
+    _______, XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_PERC, KC_7,    KC_8,         KC_9,   KC_ASTR, KC_SLSH,
+    _______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, KC_4,    KC_5,         KC_6,   KC_MINS, XXXXXXX,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_0,    KC_1,    KC_2,         KC_3,   KC_PLUS, XXXXXXX,
+    _______, _______, _______, _______, _______, _______, _______, _______, RALT(KC_ENT), KC_DOT, KC_BSPC, KC_EQL
 ),
 
 /* Raise - symbols layer
