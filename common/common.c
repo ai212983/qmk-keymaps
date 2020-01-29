1ab4dbcd9f45b3d1726e8669395a7c09b94e4cc4
Dimitri Fedorov <df.41356@gmail.com>
2020-01-29 16:15:54 +0100

    add some tap dance typography

--- common/common.c
+++ common/common.c
@@ -29,3 +29,39 @@ uint16_t get_active_layer() {
 void set_prev_layer() {
     set_persistent_layer(prev_layer);
 }
+
+// Tap Dance
+
+void dance_dot_finished (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_code (KC_DOT);
+  } else {
+    send_unicode_hex_string ("2026");
+  }
+}
+
+void dance_dot_reset (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    unregister_code (KC_DOT);
+  }
+}
+
+void dance_mins_finished (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_code (KC_MINS);
+  } else if (state->count == 2) {
+    send_unicode_hex_string ("2014"); // em dash (longer)
+  } else {
+    send_unicode_hex_string ("2013"); // en dash (shorter)
+  }
+}
+
+void dance_mins_reset (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    unregister_code (KC_MINS);
+  }
+}
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_DOT_ELLIPSIS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_dot_finished, dance_dot_reset),
+  [TD_MINS_DASH] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_mins_finished, dance_mins_reset)
+};
--- planck_ez/keymap.c
+++ planck_ez/keymap.c
@@ -112,9 +112,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_planck_grid(
     _______, XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_PERC, KC_7,    KC_8,         KC_9,   KC_ASTR, KC_SLSH,
-    _______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, KC_4,    KC_5,         KC_6,   KC_MINS, XXXXXXX,
+    _______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, KC_4,    KC_5,         KC_6,   TD_MINS, XXXXXXX,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_0,    KC_1,    KC_2,         KC_3,   KC_PLUS, XXXXXXX,
-    _______, _______, _______, _______, _______, _______, _______, _______, RALT(KC_ENT), KC_DOT, KC_BSPC, KC_EQL
+    _______, _______, _______, _______, _______, _______, _______, _______, RALT(KC_ENT), TD_DOT, KC_BSPC, KC_EQL
 ),
 
 /* Raise - symbols layer
